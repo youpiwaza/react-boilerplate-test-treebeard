325bc0439650328f6d6cbf2fcec55636a3d27f3f
youpiwaza <masamune.code@gmail.com>
2018-08-16 10:27:33 +0200

    ADD: TreeExample > doc code

--- app/components/TreeExample/index.js
+++ app/components/TreeExample/index.js
@@ -1,17 +1,66 @@
 /**
- *
  * TreeExample
  *
+ * @author       youpiwaza
+ * @description  TreeBeard example componant
+ * @see          https://github.com/storybooks/react-treebeard
+ * @version      1
  */
 
 import React from 'react';
-// import PropTypes from 'prop-types';
-// import styled from 'styled-components';
+// import ReactDOM from 'react-dom';
+import { Treebeard } from 'react-treebeard';
 
-function TreeExample() {
-  return <div />;
+const data = {
+  name: 'root',
+  toggled: true,
+  children: [
+    {
+      name: 'parent',
+      children: [{ name: 'child1' }, { name: 'child2' }],
+    },
+    {
+      name: 'loading parent',
+      loading: true,
+      children: [],
+    },
+    {
+      name: 'parent',
+      children: [
+        {
+          name: 'nested parent',
+          children: [{ name: 'nested child 1' }, { name: 'nested child 2' }],
+        },
+      ],
+    },
+  ],
+};
+
+class TreeExample extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.onToggle = this.onToggle.bind(this);
+  }
+
+  onToggle(node, toggled) {
+    if (this.state.cursor) {
+      this.state.cursor.active = false;
+    }
+
+    const pute = node;
+
+    pute.active = true;
+
+    if (node.children) {
+      pute.toggled = toggled;
+    }
+
+    this.setState({ cursor: pute });
+  }
+  render() {
+    return <Treebeard data={data} onToggle={this.onToggle} />;
+  }
 }
-
-TreeExample.propTypes = {};
 
 export default TreeExample;
