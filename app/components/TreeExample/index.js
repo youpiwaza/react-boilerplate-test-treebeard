4f10a7a621893b2b9cdee51b3773e2354ee1fe40
youpiwaza <masamune.code@gmail.com>
2018-08-16 10:10:22 +0200

    ADD: TreeExample generate

---
+++ app/components/TreeExample/index.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * TreeExample
+ *
+ */
+
+import React from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+function TreeExample() {
+  return <div />;
+}
+
+TreeExample.propTypes = {};
+
+export default TreeExample;
---
+++ app/components/TreeExample/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import TreeExample from '../index';
+
+describe('<TreeExample />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
