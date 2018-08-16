0b2b4c8c9b322683f643d9a5364428b7090f1c9b
youpiwaza <masamune.code@gmail.com>
2018-08-16 13:59:57 +0200

    MOD: extract data

--- app/components/TreeExample/index.js
+++ app/components/TreeExample/index.js
@@ -10,31 +10,7 @@
 import React from 'react';
 // import ReactDOM from 'react-dom';
 import { Treebeard } from 'react-treebeard';
-
-const data = {
-  name: 'root',
-  toggled: true,
-  children: [
-    {
-      name: 'parent',
-      children: [{ name: 'child1' }, { name: 'child2' }],
-    },
-    {
-      name: 'loading parent',
-      loading: true,
-      children: [],
-    },
-    {
-      name: 'parent',
-      children: [
-        {
-          name: 'nested parent',
-          children: [{ name: 'nested child 1' }, { name: 'nested child 2' }],
-        },
-      ],
-    },
-  ],
-};
+import data from './data.json';
 
 class TreeExample extends React.Component {
   constructor(props) {
